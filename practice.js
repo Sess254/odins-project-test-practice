a0630e857f987eaaa3ee04eeb9080b1f98769279
Sess254 <wamaritemoses@gmail.com>
2025-07-28 18:00:17 +0300

    tests for calculator object functions

--- practice.js
+++ practice.js
@@ -7,9 +7,15 @@ function reverseString(str) {
     return strToreverse.reverse().join("")
 }
 
+const calculator = {
+    add: (a,b) => a + b,
+    sub: (a,b) => a - b,
+    mul: (a,b) => a * b,
+    div: (a,b) => b !== 0 ? a / b : 'Cant divide by 0'
+}
 
 
-export {capitalize, reverseString}
+export {capitalize, reverseString, calculator}
 
 
 
--- practice.test.js
+++ practice.test.js
@@ -1,10 +1,34 @@
-import {capitalize, reverseString} from './practice'
+import {capitalize, 
+        reverseString,
+        calculator } from './practice'
 
-
-it('takes a string and captitalizes first character', () => {
-    expect(capitalize('cat')).toEqual('Cat')
+describe('capitalize', () => { 
+    it('takes a string and captitalizes first character', () => {
+        expect(capitalize('cat')).toEqual('Cat')
+    })
 })
 
-it('takes a string and reverses it', ()=> {
-    expect(reverseString('hello')).toEqual('olleh')
+describe('reversString', ()=> {
+    it('takes a string and reverses it', ()=> {
+        expect(reverseString('hello')).toEqual('olleh')
+    })
+
+})
+
+describe('calculator', () => {
+    it('adds two numbers', () => {
+        expect(calculator.add(2,3)).toBe(5)
+    })
+
+    it('subracts two numbers', () => {
+        expect(calculator.sub(4, 2)).toBe(2)
+    })
+
+    it('multiplies two numbers', () => {
+        expect(calculator.mul(4,2)).toBe(8)
+    })
+
+    it('divides two numbers', () => {
+        expect(calculator.div(4,2)).toBe(2)
+    })
 })
